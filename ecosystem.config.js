11c0af5c726e261a53507e45339daec0a007aca3
Gideon Ibitoye <ibitoyrgideon79@gmail.com>
2026-04-27 15:25:44 +0100

    Create ecosystem.config.js added

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'chiefbaranda',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+      instances: 1,
+      exec_mode: 'fork',
+      max_memory_restart: '500M',
+      error_file: '/var/log/chiefbaranda-error.log',
+      out_file: '/var/log/chiefbaranda-out.log',
+    },
+  ],
+};
